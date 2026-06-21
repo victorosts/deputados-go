206c4710fb899a1813087023a14308b57b90d0ee
Victor Santos <victorstsoli@gmail.com>
2026-06-21 11:35:06 -0300

    Feat: Integrando com api de deputados

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("start point")
+
+	resp, err := http.Get("https://dadosabertos.camara.leg.br/api/v2/deputados")
+	if err != nil {
+		fmt.Printf("Application ended with error: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error while reading response body: %s", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
